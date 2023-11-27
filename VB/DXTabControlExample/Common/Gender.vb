202851b5315af989b3a73fb199c44023669992a9
DevExpressExampleBot <fakeEmail>
2023-11-27 18:07:49 +0400

    Source auto update [skip ci]

--- VB/DXTabControlExample/Common/Gender.vb
+++ VB/DXTabControlExample/Common/Gender.vb
@@ -1,9 +1,5 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-
 Namespace DXTabControlExample.Common
+
     Public Enum Gender
         Female = 0
         Male = 1
